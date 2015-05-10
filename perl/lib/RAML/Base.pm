6826660d66a1af01b3631e00b242a1e29c17aca6
tquintela <quintela.tiago@gmail.com>
2015-05-10 18:49:34 +0100

    bootstrap OO classes && update gitignore

---
+++ perl/lib/RAML/Base.pm
@@ -0,0 +1,31 @@
+package RAML::Base;
+
+=head1 NAME
+
+RAML::Base
+
+=head1 SYNOPSIS
+
+=head1 VERSION
+  
+  0.10
+
+=head1 DESCRIPTION
+
+=head1 INTERFACE
+=cut
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Tiago Quintela, <quintela[at]....pt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2015 by Tiago Quintela
+
+This library is free software; you can redistribute it and/or modify it 
+under the same terms as Perl itself.
